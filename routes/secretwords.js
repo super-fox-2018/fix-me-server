7159b204b0c02175d07e4b407640b1dbb8b5ea73
David Joshua <davidjosh_87@yahoo.com>
2018-07-05 11:20:03 +0700

    push lgi

--- routes/secretwords.js
+++ routes/secretwords.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
-const { postWords } = require('../controllers/secretword')
+const {
+  postWords
+} = require('../controllers/secretword')
 /* GET users listing. */
 router.post('/', postWords)
 
